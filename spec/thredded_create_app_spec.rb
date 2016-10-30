523c5bde2cde2e28fa246e70fce319fae396c8d5
Gleb Mazovetskiy <glex.spb@gmail.com>
2016-10-29 20:10:04 -0700

    add todo on tests

--- spec/thredded_create_app_spec.rb
+++ spec/thredded_create_app_spec.rb
@@ -9,5 +9,7 @@ describe ThreddedCreateApp do
   it 'generates the app without errors' do
     expect(system('bin/create-tmp-myapp --verbose --no-start-server'))
       .to(be true)
+    # TODO: Add tests to the app that check 200 status on home and forums and
+    # run them here.
   end
 end
