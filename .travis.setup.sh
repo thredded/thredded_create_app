7f8aaeceb238949b435d9a09cddf2d50e91317eb
Gleb Mazovetskiy <glex.spb@gmail.com>
2016-11-18 02:52:48 +0000

    travis

--- .travis.setup.sh
+++ .travis.setup.sh
@@ -15,5 +15,5 @@ if [ "$DB" = 'mysql2' ]; then
   sudo apt-get update -qq
   sudo apt-get install -qq mysql-server libmysqlclient-dev
   # https://www.percona.com/blog/2016/03/16/change-user-password-in-mysql-5-7-with-plugin-auth_socket/
-  mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY ''"
+  sudo mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY ''"
 fi
