e2d475b31c0bcb76fda899a9bf8deac96a98c796
Gleb Mazovetskiy <glex.spb@gmail.com>
2016-12-04 20:13:01 +0000

    travis

--- .travis.setup.mysql_v5_7.sh
+++ .travis.setup.mysql_v5_7.sh
@@ -8,4 +8,4 @@ add-apt-repository 'deb http://repo.mysql.com/apt/ubuntu/ trusty mysql-5.7'
 apt-get update -qq
 apt-get install -qq mysql-server libmysqlclient-dev
 # https://www.percona.com/blog/2016/03/16/change-user-password-in-mysql-5-7-with-plugin-auth_socket/
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY ''"
+mysql -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('')"
