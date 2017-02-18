b9809c69b49d5169dd78dd4e28d63281140bcf37
Gleb Mazovetskiy <glex.spb@gmail.com>
2017-02-18 17:50:56 +0000

    Update .travis.setup.mysql_v5_7.sh

--- .travis.setup.mysql_v5_7.sh
+++ .travis.setup.mysql_v5_7.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-
 # Install MySQL v5.7. Must be run as root.
-# Copied from https://github.com/brianmario/mysql2/blob/40cae130b6d183dd4a1911dbc41791b04f1930aa/.travis_mysql57.sh
+# Based on  https://github.com/brianmario/mysql2/blob/220c3ff70c5aa831754cb045ec153182b0ea0f41/.travis_mysql57.sh
+set -eux
 apt-get purge -qq '^mysql*' '^libmysql*'
+rm -rf /etc/mysql
+rm -rf /var/lib/mysql
 apt-key adv --keyserver pgp.mit.edu --recv-keys 5072E1F5
 add-apt-repository 'deb http://repo.mysql.com/apt/ubuntu/ trusty mysql-5.7'
 apt-get update -qq
