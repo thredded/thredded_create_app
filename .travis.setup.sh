0069482a5a5d0a9bb0e2c8a00e21a23bfb58c1bb
Gleb Mazovetskiy <glex.spb@gmail.com>
2018-07-01 23:17:47 +0100

    travis: Use pre-installed MySQL 5.6

--- .travis.setup.sh
+++ .travis.setup.sh
@@ -6,7 +6,3 @@ if [ "$DB" = 'postgresql' ]; then
   sudo service postgresql start
   psql -d template1 -c 'CREATE EXTENSION citext;' -U postgres
 fi
-
-if [ "$DB" = 'mysql2' ]; then
-  sudo bash .travis.setup.mysql_v5_7.sh
-fi
